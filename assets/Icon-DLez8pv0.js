fc4fcb5aa5fd723b12065e1aea7a6588f28d664f
gabimolocea <gabimolocea@gmail.com>
2026-01-08 17:01:46 +0200

    Updates

---
+++ assets/Icon-DLez8pv0.js
@@ -0,0 +1,8 @@
+const o=`<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M18.75 4.75H0.75V18.75H18.75V4.75Z" stroke="#6C6C72" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M6.25 14.25C7.63071 14.25 8.75 13.1307 8.75 11.75C8.75 10.3693 7.63071 9.25 6.25 9.25C4.86929 9.25 3.75 10.3693 3.75 11.75C3.75 13.1307 4.86929 14.25 6.25 14.25Z" stroke="#6C6C72" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M11.75 10.25H14.75" stroke="#6C6C72" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M11.75 13.25H14.75" stroke="#6C6C72" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M0.75 4.5492L10.1196 0.75" stroke="#6C6C72" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;export{o as default};
